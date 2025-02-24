392d78de2ee76f7c1946e3ac16863f2b03014111
celkecursos <cesar@celke.com.br>
2025-02-24 17:58:09 -0300

    Como criar o formulário cadastrar com Laravel 12

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    // Carregar o formulário cadastrar novo usuário
+    public function create()
+    {
+        // Carregar a VIEW
+        return view('users.create');
+    }
+
+    // Cadastrar no banco de dados o novo registro
+    public function store(Request $request)
+    {
+        // dd($request);
+
+        try {
+
+            // Cadastrar no banco de dados na tabela usuários
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password
+            ]);
+
+            // Redirecionar o usuário, enviar a mensagem de sucesso
+            return redirect()->route('user.create')->with('success', 'Usuário cadastrado com sucesso!');
+
+        } catch (Exception $e) {
+
+            // Redirecionar o usuário, enviar a mensagem de erro
+            return back()->withInput()->with('error', 'Usuário não cadastrado!');
+        }
+    }
+}
