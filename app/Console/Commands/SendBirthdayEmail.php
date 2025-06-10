90ea57b56ae0b07fd53cdcc8274ca471b21e05c1
Cesar Szpak <cesar@celke.com.br>
2025-06-10 13:33:24 -0300

    #27 - Agendar envio diário de e-mail de aniversário no Laravel 12 com CRON na VPS Hostinger

---
+++ app/Console/Commands/SendBirthdayEmail.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\BirthdayEmail;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class SendBirthdayEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-birthday-email';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Envia e-mails de aniversário para os usuários aniversariantes do dia';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // Recuperar a data atual
+        $today = Carbon::today();
+
+        // Filtrar usuários cujo dia e mês de nascimento correspondem ao dia atual
+        $users = User::whereMonth('date_of_birth', $today->month)
+            ->whereDay('date_of_birth', $today->day)
+            ->get();
+
+        // Laço de repetição para ler os registros retornado do banco de dados
+        foreach ($users as $user) {
+            Mail::to($user->email)->send(new BirthdayEmail($user));
+            $this->info("E-mail enviado para: {$user->name}");
+        }
+
+        return Command::SUCCESS;
+    }
+}
---
+++ resources/views/emails/birthday.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Celke</title>
+</head>
+
+<body>
+
+    <h1>Feliz aniversário, {{ $user->name }}! 🎉</h1>
+    
+    <p>Desejamos a você um dia maravilhoso e cheio de alegrias.</p>
+
+</body>
+
+</html>
